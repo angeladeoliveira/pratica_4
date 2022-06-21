caa5042c0d8c3dd4b82032a7a5996bae37943cb7
Angela de Oliveira <aho.angela@gmail.com>
2022-06-21 18:05:20 -0300

    Add files

--- app/src/main/java/com/example/pratica_4/ListaActivity.kt
+++ app/src/main/java/com/example/pratica_4/ListaActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pratica_4
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -9,13 +10,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListaActivity: AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_lista)
         setSupportActionBar(findViewById(R.id.tbrLista))
-        setTitle("My recipes")
+        title = "My recipes"
 
-        val recipes = arrayOf("Hamburger", "Pizza", "Caesar Salad")
+        val recipes = arrayOf("\nHamburger \nThe best burger recipe for summertime grilling.\n", "\nPizza \nAn easy pizza crust recipe that will hold all your favorite toppings.\n", "\nCaesar Salad \nYour new favorite caesar salad, thanks to one secret ingredient.\n", "\nHomemade rocky road ice cream \nHomemade rocky road ice cream is summer in a bowl.\n")
         var adaptador = ArrayAdapter<String>(this,
             android.R.layout.simple_list_item_1, recipes)
 
@@ -26,6 +28,14 @@ class ListaActivity: AppCompatActivity() {
         botao.setOnClickListener {
             val intencao = Intent(applicationContext, CadastroActivity::class.java)
             startActivity(intencao)
+        }
+
+        botao.setOnLongClickListener{
+            return@setOnLongClickListener false
+        }
+
+        botao.setOnTouchListener{ view, motionEvent ->
+            return@setOnTouchListener false
         }
     }
 
--- app/src/main/java/com/example/pratica_4/LoginActivity.kt
+++ app/src/main/java/com/example/pratica_4/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.pratica_4
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_login)
@@ -14,6 +18,15 @@ class LoginActivity : AppCompatActivity() {
         botao.setOnClickListener {
             val intencao = Intent(applicationContext, ListaActivity::class.java)
             startActivity(intencao)
+        }
+
+        val campo = findViewById<EditText>(R.id.edtEmail)
+        val campo2 = findViewById<EditText>(R.id.edtSenha)
+        campo.setOnKeyListener{ view, i, keyEvent ->
+            if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
+                campo2.requestFocus()
+                return@setOnKeyListener true
+            } else false
         }
     }
 }
